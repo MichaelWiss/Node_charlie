af4ff578fa312aa19503bbda98c7102a24ea80b8
Michael Wiss <michael.wiss@gmail.com>
2015-10-14 21:06:40 -0500

    event emitter

--- spec/FilesizeWatcherSpec.js
+++ spec/FilesizeWatcherSpec.js
@@ -21,36 +21,36 @@
    expect(gain).toBe(5); done();
 });
 
-exec('echo "test" > ' + path, function() {});
+   exec('echo "test" > ' + path, function() {});
 
 });
 
 });
 
-it('should fire a "shrank" event when the file grew in size', function(done) {
+   it('should fire a "shrank" event when the file grew in size', function(done) {
 
 var path = '/var/tmp/filesizewatcher.test';
-exec('rm -f ' + path + ' ; echo "test" > ' + path, function() {
-watcher = new FilesizeWatcher(path);
+   exec('rm -f ' + path + ' ; echo "test" > ' + path, function() {
+    watcher = new FilesizeWatcher(path);
 
 watcher.on('shrank', function(loss) {
-expect(loss).toBe(3);
-done();
+   expect(loss).toBe(3);
+   done();
 });
 
-exec('echo "a" > ' + path, function() {});
+   exec('echo "a" > ' + path, function() {});
 
 });
 
 });
 
-it('should fire "error" if path does not start with a slash', function(done) {
+   it('should fire "error" if path does not start with a slash', function(done) {
 
 var path = 'var/tmp/filesizewatcher.test';
 watcher = new FilesizeWatcher(path);
 
 watcher.on('error', function(err) {
-expect(err).toBe('Path does not start with a slash');
+    expect(err).toBe('Path does not start with a slash');
 done();
 });
 
