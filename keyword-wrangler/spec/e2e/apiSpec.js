a71b374d1f67c8a22e5e1d7c9914d3db3c35015c
Michael Wiss <michael.wiss@gmail.com>
2015-10-21 14:00:59 -0500

    added db spec

--- keyword-wrangler/spec/e2e/apiSpec.js
+++ keyword-wrangler/spec/e2e/apiSpec.js
@@ -5,6 +5,19 @@ var request = require('request');
 describe('The API', function() {
 	
 	it('should respond to a GET request at /api/keywords/', function (done){
+       
+       var expected = {
+
+       	"_items": [
+           {'id': 1, 'value': 'Aubergine', 'category': 1},
+           {'id': 2, 'value': 'Onion', 'category': 1},
+           {'id': 3, 'value': 'Knife', 'category': 2}
+
+       	]
+       }
+
+
+
 	   request.get(
 	   	{
 	   	  'url': 'http://localhost:8080/api/keywords/', 
@@ -12,7 +25,7 @@ describe('The API', function() {
 	   	},
 	   	function (err, res, body) {
 	   		expect(res.statusCode).toBe(200);
-	   		expect(body.foo).toEqual('bar');
+	   		expect(body.foo).toEqual(expected);
 	   		done();
 	   	});
 	});
