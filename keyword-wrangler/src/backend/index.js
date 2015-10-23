a06fde15f826078554ae464fad768ac2b55b4888
Michael Wiss <michael.wiss@gmail.com>
2015-10-23 14:47:34 -0500

    index.js

--- keyword-wrangler/src/backend/index.js
+++ keyword-wrangler/src/backend/index.js
@@ -1,17 +1,26 @@
 'use strict';
 
 var Percolator = require('percolator').Percolator;
+var dbSession = require('../../src/backend/dbSession.js');
 
 var port = 8080;
-var server = Percolator({'port': port});
+var server = Percolator({'port': port, 'autolink': false});
 
 server.route('/api/keywords',
    {
-     GET: function (req, res) {
-        res.object({'foo': 'bar'}).send()
+   Get: function (req, res) {
+   	dbSession.fetchALL('SELECT id, value, categoryID FROM keyword ORDER BY id'\,
+   	  function(err, rows) {
+   	  	if(err) {
+   	  		console.log(err);
+   	  		res.status.internalServerError(err);
+   	  	} else{
+   	  		res.collection(rows).send();
+   	  	}
+   	  });
+   		}
      }
-   }
-);
+   );
 
 server.listen(function() {
 	console.log('Server started and listening on port', port);
